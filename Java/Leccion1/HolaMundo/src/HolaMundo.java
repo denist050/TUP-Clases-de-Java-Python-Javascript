2da641fe7185d3bb910eccc925c7291f0ef8b46e
Denis Taraboreli <dtaraboreli@gmail.com>
2024-05-16 12:05:14 -0300

    Creamos la carpeta Leccion1 y la clase HolaMundo en Java. Realizamos el primer ejercicio de Java.

---
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,9 @@
+//Mi clase en Java
+/*
+Usamos /* para escribir muchas lineas
+*/
+public class HolaMundo {
+    public static void main(String args []){
+        System.out.println("Hola mundo desde Java");
+    }
+}
