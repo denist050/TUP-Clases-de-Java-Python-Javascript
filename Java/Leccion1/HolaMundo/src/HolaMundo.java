92900726e1dd0669a1a0f192c07a1ad1d807364a
Denis Taraboreli <dtaraboreli@gmail.com>
2024-05-17 00:06:54 -0300

    Agregamos el ejercicio de la clase 3 - Var - inferencia de tipos en java.

--- Java/Leccion1/HolaMundo/src/HolaMundo.java
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -4,7 +4,7 @@ Usamos /* para escribir muchas lineas
 */
 public class HolaMundo {
     public static void main(String args []){
-        System.out.println("Hola mundo desde Java");
+        /*System.out.println("Hola mundo desde Java");
         
         int miVariable = 10;
         System.out.println(miVariable);
@@ -12,13 +12,24 @@ public class HolaMundo {
         System.out.println(miVariable);
         //Tipo String
         String miVariableCadena = "bienvenidos";
-        /*
+        
         Las variables tipo cadena tienen privilegios
         Si fuese un objeto se utiliza "new String("Bienvenidos");"
-        */
+        
         System.out.println(miVariableCadena);
         miVariableCadena = "Sigamos creciendo en Java";
-        System.out.println(miVariableCadena); 
-        //Control + click izquierdo me donde esta instanciada la variable.
+        System.out.println(miVariableCadena);
+        Control + click izquierdo me donde esta instanciada la variable.
+        */
+        
+        //Var -inferencia de tipos en Java
+        var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2); 
+        //soutv + tab
+        
+          
+               
     }
 }
