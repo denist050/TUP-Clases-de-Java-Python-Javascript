77c9041bd8a5cf0a53b7ee9969741c518332738f
Denis Taraboreli <dtaraboreli@gmail.com>
2024-06-24 14:52:38 -0300

    Terminamos clase 7 Operadores de JavaScript

--- JavaScript/leccion02/02-01-ConcatenacionDeCadena.js
+++ JavaScript/leccion02/02-01-ConcatenacionDeCadena.js
@@ -13,3 +13,24 @@ console.log(juntos);
 
 nombre += ' ' + apellido; //concatenacion usando el operador simplificado
 console.log(nombre);
+
+// Hoy ya no se usa var, se utiliza let y const
+let nombre2 = 'Pedro';
+console.log(nombre2);
+
+const apellido2 = 'Lepes';
+// apellido2 = 'Peres'; una constante no puede ser modificada
+console.log(apellido2); 
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; //se puede hacer asignacion de varias variables dentro de una misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+//javascrip no permite comenzar la variable con un un numero  
+
+let _1num = 31;
+let rompiendo = "rompe"; //no utilizar palablas reservadas para variables como break
+
+console.log(_1num);
+console.log(rompiendo);
---
+++ JavaScript/leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar numeros pares
+let parImpar = 1;
+if(parImpar % 2 == 0){
+    console.log("Es un numero PAR");
+}
+else{
+    console.log("Es un numero IMPAR");
+}
+
+//Ejercico: es mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Es mayor de edad")
+}
+else{
+    console.log("Es menor de edad")
+}
